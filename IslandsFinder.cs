e0f0bb2dfc2dde10401a36102f2542ef44d95731
Maksym Tatsenko <maksym.tatsenko@skylum.com>
2021-09-16 16:42:37 +0300

    Commit

---
+++ IslandsFinder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class IslandsFinder
+    {
+        private readonly int[] _x = new int[] { -1, -1, 0, 1, 1, 1, 0, -1 };
+        private readonly int[] _y = new int[] { 0, 1, 1, 1, 0, -1, -1, -1 };
+
+        public uint FindMaxIslandSize(int[,] matrix)
+        {
+            uint res = 0;
+            for (int i = 1; i < matrix.GetLength(0) - 1; i++)
+            {
+                for (int j = 1; j < matrix.GetLength(1) - 1; j++)
+                {
+                    if (matrix[i, j] != 2)
+                    {
+                        uint s = 0;
+                        Dfs(matrix, i, j, ref s);
+                        res = Math.Max(s, res);
+                    }
+                }
+            }
+
+            return res;
+        }
+
+        private void Dfs(int[,] matrix, int x, int y, ref uint size)
+        {
+            matrix[x, y] = 2;
+            for (int i = 0; i < _x.Length; i++)
+            {
+                var newX = x + _x[i];
+                var newY = y + _y[i];
+
+                if (matrix[newX, newY] == 1)
+                {
+                    size += 1;
+                    Dfs(matrix, newX, newY, ref size);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var matrix = new int[,]
+            {
+                { 2, 2, 2, 2, 2, 2, 2, 2, 2 },
+                { 2, 0, 0, 0, 0, 1, 0, 1, 2 },
+                { 2, 0, 1, 1, 0, 0, 1, 1, 2 },
+                { 2, 0, 0, 1, 0, 0, 0, 1, 2 },
+                { 2, 0, 0, 0, 1, 0, 0, 0, 2 },
+                { 2, 0, 1, 0, 0, 0, 0, 0, 2 },
+                { 2, 2, 2, 2, 2, 2, 2, 2, 2 },
+            };
+            var finder = new IslandsFinder();
+            var res = finder.FindMaxIslandSize(matrix);
+        }
+    }
+}
